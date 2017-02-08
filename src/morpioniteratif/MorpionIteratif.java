b9ceb4b5016e14f9b5d22b1f281b3c5f58c283f7
Clément Serrano <clementserrano3@gmail.com>
2017-02-08 01:32:10 +0100

    encore quelques bug

--- src/morpioniteratif/MorpionIteratif.java
+++ src/morpioniteratif/MorpionIteratif.java
@@ -13,136 +13,264 @@ import java.util.Scanner;
  */
 public class MorpionIteratif {
 
+    private static String[][] grille;
+    private static Scanner reader;
+    private static String input;
+
+    private static int x;
+    private static int y;
+
+    private static int x2;
+    private static int y2;
+    private static int n;
+    private static int x3;
+    private static int y3;
+
+    private static String player;
+    private static String ia;
+    private static String empty;
+
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        String[][] grille = new String[3][3];
-        Scanner reader = new Scanner(System.in);
-        String input = "";
-        int x = 0;
-        int y = 0;
-        
-        initGrille(grille);
-        
-        while(!isFilled(grille)){
-            x=-1;
-            y=-1;
-            while(x<0 || x>=grille.length || y<0 || y>=grille.length){
+        // Initialisation des variables
+        grille = new String[3][3];
+        reader = new Scanner(System.in);
+        input = "";
+
+        x = 0;
+        y = 0;
+
+        x2 = 0;
+        y2 = 0;
+        n = 0;
+        x3 = 0;
+        y3 = 0;
+
+        player = "o";
+        ia = "x";
+        empty = "-";
+
+        initGrille();
+
+        // Boucle de la partie
+        while (!ligneRemplie()) {
+            x = -1;
+            y = -1;
+
+            // Utilisateur
+            while (x < 0 || x >= grille.length || y < 0 || y >= grille.length
+                    || (x >= 0 & x < grille.length & y >= 0 && y < grille.length
+                    && !grille[x][y].equals(empty))) {
                 System.out.print("Entrez votre coup : ");
                 input = reader.nextLine();
-                x = Integer.parseInt(""+input.charAt(0));
-                y = Integer.parseInt(""+input.charAt(2));
-            }
-            
-            grille[x][y]="o";
-            
-            printGrille(grille);
-            
-            // IA
-            int[] res;
-            // l'IA vérifie si coup gagnant
-            res = verifCoup(grille, "x","o");
-            if(res[0]!=-1 || res[1] != -1){
-                grille[res[0]][res[1]] = "x";
-            }else{ // Sinon IA vérifie si contre
-                res = verifCoup(grille, "o","x");
-                if(res[0]!=-1 || res[1] != -1){
-                    grille[res[0]][res[1]] = "x";
-                }else{ // Sinon IA joue random
-                    int x2 = 0;
-                    int y2 = 0;
-                    do{
-                        x2 = (int)(Math.random() * (2-0)) + 0;
-                        y2 = (int)(Math.random() * (2-0)) + 0;
-                    }while(grille[x2][y2].equals("-"));
-                    grille[x2][y2]="x";
+                try {
+                    x = Integer.parseInt("" + input.charAt(0));
+                    y = Integer.parseInt("" + input.charAt(2));
+                } catch (Exception e) {
+                    x = -1;
+                    y = -1;
                 }
+            }
+
+            // Coup joué
+            grille[x][y] = player;
+
+            // Affiche la grille
+            printGrille();
+
+            // IA
+            x2 = -1;
+            y2 = -1;
+
+            // l'IA vérifie si coup gagnant ou contre (x2 et y2 changés)
+            verifCoup();
+
+            // Coup random (x2 et y2 non changés)
+            if (x2 == -1 && y2 == -1) {
+
+                do {
+                    x2 = (int) (Math.random() * (3 - 0)) + 0;
+                    y2 = (int) (Math.random() * (3 - 0)) + 0;
+                } while (!grille[x2][y2].equals(empty));
+            }
+
+            // Coup joué
+            grille[x2][y2] = ia;
+
+            // Affiche la grille
+            printGrille();
+
+            if (n == 2) { // L'IA a joué un coup gagnant
+                System.out.println("IA gagne");
+                System.exit(0); // On arrête le jeu
             }
         }
     }
-    
-    private static boolean isFilled(String[][] grille){
-        for(int i = 0; i< grille.length;i++){
-            for(int j = 0; j<grille[0].length;j++){
-                if(grille[i][j].equals("-")){
+
+    private static void verifCoup() {
+        // Si n == 2, coup gagnant, on sort de la fonction et on joue
+        // Si n == -2, coup contre, on continue de vérifier si il y a un coup gagnant
+        // A la fin de la vérification, si x2 et y2 sont égaux à -1 cela veut dire qu'il
+        // n'y a pas de coup contre donc coup random
+
+        x2 = -1;
+        y2 = -1;
+        x3 = -1;
+        y3 = -1;
+
+        // Colonnes
+        for (int j = 0; j < grille[0].length; j++) {
+            n = 0;
+            for (int i = 0; i < grille.length; i++) {
+                if (grille[i][j].equals(ia)) {
+                    n++;
+                } else if (grille[i][j].equals(player)) {
+                    n--;
+                } else if (grille[i][j].equals(empty)) {
+                    x2 = i;
+                    y2 = j;
+                }
+            }
+
+            switch (n) {
+                case 2: // Coup gagnant -> on arrête la vérification
+                    return;
+                case -2: // Coup contre -> on enregistre le coup
+                    x3 = x2;
+                    y3 = y2;
+                    break;
+                default: // Pas de coup contre -> on réinitialise le coup
+                    x2 = -1;
+                    y2 = -1;
+                    break;
+            }
+        }
+
+        // Lignes
+        for (int i = 0; i < grille.length; i++) {
+            n = 0;
+            for (int j = 0; j < grille[0].length; j++) {
+                if (grille[i][j].equals(ia)) {
+                    n++;
+                } else if (grille[i][j].equals(player)) {
+                    n--;
+                } else if (grille[i][j].equals(empty)) {
+                    x2 = i;
+                    y2 = j;
+                }
+            }
+
+            switch (n) {
+                case 2: // Coup gagnant -> on arrête la vérification
+                    return;
+                case -2: // Coup contre -> on enregistre le coup
+                    x3 = x2;
+                    y3 = y2;
+                    break;
+                default: // Pas de coup contre -> on réinitialise le coup
+                    x2 = -1;
+                    y2 = -1;
+                    break;
+            }
+
+        }
+
+        // Diagonales
+        int i = 0;
+        int j = 0;
+
+        // 1ère diagonale
+        n = 0;
+        while (i < grille.length && j < grille[0].length) {
+            if (grille[i][j].equals(ia)) {
+                n++;
+            } else if (grille[i][j].equals(player)) {
+                n--;
+            } else if (grille[i][j].equals(empty)) {
+                x2 = i;
+                y2 = j;
+            }
+            i++;
+            j++;
+        }
+
+        switch (n) {
+            case 2: // Coup gagnant -> on arrête la vérification
+                return;
+            case -2: // Coup contre -> on enregistre le coup
+                x3 = x2;
+                y3 = y2;
+                break;
+            default: // Pas de coup contre -> on réinitialise le coup
+                x2 = -1;
+                y2 = -1;
+                break;
+        }
+
+        // 2ème diagonale
+        n = 0;
+        while (i < grille.length && j < grille[0].length) {
+            if (grille[i][grille[0].length - j].equals(ia)) {
+                n++;
+            } else if (grille[i][grille[0].length - j].equals(player)) {
+                n--;
+            } else if (grille[i][grille[0].length - j].equals(empty)) {
+                x2 = i;
+                y2 = j;
+            }
+            i++;
+            j++;
+        }
+
+        switch (n) {
+            case 2: // Coup gagnant -> on arrête la vérification
+                return;
+            case -2: // Coup contre -> on enregistre le coup
+                x3 = x2;
+                y3 = y2;
+                break;
+            default: // Pas de coup contre -> on réinitialise le coup
+                x2 = -1;
+                y2 = -1;
+                break;
+        }
+
+        // Fin de la vérification
+        // Pas de coup gagnant
+        // Si x3 et y3 sont différents de -1 -> coup contre
+        if (x3 != -1 && y3 != -1) {
+            x2 = x3;
+            y2 = y3;
+        }
+    }
+
+    private static void initGrille() {
+        for (int i = 0; i < grille.length; i++) {
+            for (int j = 0; j < grille[0].length; j++) {
+                grille[i][j] = empty;
+            }
+        }
+    }
+
+    private static boolean ligneRemplie() {
+        for (int i = 0; i < grille.length; i++) {
+            for (int j = 0; j < grille[0].length; j++) {
+                if (grille[i][j].equals(empty)) {
                     return false;
                 }
             }
         }
         return true;
     }
-    
-    private static void printGrille(String[][] grille){
-        for(int i = 0; i< grille.length;i++){
-            for(int j = 0; j<grille[0].length;j++){
-                System.out.print(grille[i][j]+" ");
+
+    private static void printGrille() {
+        for (int i = 0; i < grille.length; i++) {
+            for (int j = 0; j < grille[0].length; j++) {
+                System.out.print(grille[i][j] + " ");
             }
             System.out.println("");
         }
-    }
-    
-    private static void initGrille(String[][] grille){
-        for(int i = 0; i< grille.length;i++){
-            for(int j = 0; j<grille[0].length;j++){
-                grille[i][j]="-";
-            }
-        }
-    }
-    
-    private static int[] verifCoup(String[][] grille, String motif, String motifEnnemi){
-        int x = -1;
-        int y = -1;
-        
-        int[] res = verifColonnes(grille,motif,motifEnnemi);
-        if(res[0]!=-1 || res[1] != -1){
-            return new int[]{x,y};
-        }
-        
-        res = verifLignes(grille,motif,motifEnnemi);
-        if(res[0]!=-1 || res[1] != -1){
-            return new int[]{x,y};
-        }
-        
-        res = verifDiagonales(grille,motif,motifEnnemi);
-        if(res[0]!=-1 || res[1] != -1){
-            return new int[]{x,y};
-        }
-        
-        return new int[]{x,y};
-    }
-    
-    private static int[] verifColonnes(String[][] grille, String motif, String motifEnnemi){
-        int x = -1;
-        int y = -1;
-        int n = 0;
-        
-        for(int j = 0; j< grille[0].length;j++){
-            n = 0;
-            for(int i = 0; i<grille.length;i++){
-                if(grille[i][j].equals(motif)){
-                    n++;
-                }else if(grille[i][j].equals(motifEnnemi)){
-                    n--;
-                }
-            }
-            
-        }
-        
-        return new int[]{x,y,n};
-    }
-    
-    private static int[] verifLignes(String[][] grille, String motif, String motifEnnemi){
-        int x = -1;
-        int y = -1;
-        
-        return new int[]{x,y};
-    }
-    
-    private static int[] verifDiagonales(String[][] grille, String motif, String motifEnnemi){
-        int x = -1;
-        int y = -1;
-        
-        return new int[]{x,y};
     }
 }
